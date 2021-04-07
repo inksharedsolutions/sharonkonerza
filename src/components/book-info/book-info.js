31894548bc56bae3d8e8a226390c1c8125c93476
inksharedsolutions <57968903+inksharedsolutions@users.noreply.github.com>
2021-04-08 02:15:15 +0800

    Update book-info.js

--- src/components/book-info/book-info.js
+++ src/components/book-info/book-info.js
@@ -56,7 +56,7 @@ const BookInfo = (props)=>{
                         <nav className="booklinkBlocks">
                             <span>Ebooks :</span>
                             <li><a rel="noopener noreferrer" target="_blank" href={props.data.ebooks.amazon}>Amazon</a></li>
-                            <li><a rel="noopener noreferrer" target="_blank" href={props.data.ebooks.stratton}>Stratton Press</a></li>
+                            {/*<li><a rel="noopener noreferrer" target="_blank" href={props.data.ebooks.stratton}>Stratton Press</a></li>*/}
                             <li><a rel="noopener noreferrer" target="_blank" href={props.data.ebooks.barnes}>Barnes & Noble</a></li>
                         </nav>
 
